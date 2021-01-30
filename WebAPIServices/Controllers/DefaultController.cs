aceb5be687a4db2348bfd74fcf9ee2d3487bc271
Srinivas Madarapu <sri.nivas006@gmail.com>
2021-01-30 17:08:18 +0530

    CRUD operation testing with List<string>

---
+++ WebAPIServices/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIServices.Controllers
+{
+    public class DefaultController : ApiController
+    {
+
+        public static List<string> collection = new List<string>()
+        {
+            "Value 1",
+            "Value 2",
+            "Value 3"
+        };
+        
+        public IEnumerable<string> Get()
+        {
+            return collection;
+        }
+
+        public string Get(int Id)
+        {
+            return collection[Id];
+        }
+
+        public void Post([FromBody]string value)
+        {
+            collection.Add(value);
+        }
+
+        public void Put(int Id, [FromBody]string value)
+        {
+            collection[Id] = value;
+        }
+
+        public void Delete(int Id)
+        {
+            collection.RemoveAt(Id);
+        }
+    }
+}
